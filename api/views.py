7f879f8e147895daf9d6ffe9afb989628846bd95
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2023-04-14 00:43:58 +0900

    update requirements.txt, api/views.py, .gitignor:04.14

--- api/views.py
+++ api/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CaptionImageSerializer
 from googletrans import Translator
-from gtts import gTTS
 import replicate
 
 translator = Translator()
@@ -30,32 +29,6 @@ class CaptionImageUploadView(APIView):
             ).text
             # time : 3.79s
 
-            # sp = gTTS(lang='ko', text=trans_output, slow=False)
-            # audio_file = f'media/sph/{serializer.data["id"]}.mp3'
-            # sp.save(audio_file)
-            # # time : 16.65s
-
             return Response({'text': trans_output}, status=201)
         else:
             return Response(serializer.errors, status=400)
-
-
-def caption_view(request):
-    serializer = CaptionImageSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-        # Run the image through the model
-        # output = replicate.run(
-        #     "rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
-        #     input={"image": open(f'{serializer.data["image"]}', 'rb')}
-        # )
-        # print(output)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    # # Convert the output text to audio
-    # sp = gTTS(lang='en', text=output, slow=False)
-    # audio_file = 'image_caption.mp3'
-    # sp.save(audio_file)
-
-    return Response({'error': 'No image file provided.'}, status=400)
