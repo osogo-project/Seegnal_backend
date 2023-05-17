f253f503fb5e10de713825522859607b60e6ed33
oshmos <ojames9707@gmail.com>
2023-05-17 10:13:33 +0900

    deploy seegnal_v2 through pythonanywhere:paas - 05.17

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,7 @@
-from .views import CaptionImageUploadView, CaptionImageUploadENView, ImageCaptioningKRView, ImageCaptioningENView
+from .views import ImageCaptioningKRView, ImageCaptioningENView
 from django.urls import path
 
 urlpatterns = [
-    path('v1/caption', CaptionImageUploadView.as_view(), name='caption'),
-    path('v1/caption_en', CaptionImageUploadENView.as_view(), name='caption_en'),
-    path('v2/caption_kr', ImageCaptioningKRView.as_view()),
-    path('v2/caption_en', ImageCaptioningENView.as_view()),
+    path('v2/caption_kr', ImageCaptioningKRView.as_view(), name='caption_kr'),
+    path('v2/caption_en', ImageCaptioningENView.as_view(), name='caption_en'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,5 @@
 import os
-import json
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CaptionImageSerializer
@@ -58,45 +56,6 @@ class ImageCaptioningENView(APIView):
                     "use_beam_search": True
                 }
             )
-            return Response({'text': output}, status=201)
-        else:
-            return Response(serializer.errors, status=400)
-
-
-class CaptionImageUploadView(APIView):
-    parser_classes = [MultiPartParser, FormParser]
-
-    def post(self, request):
-        serializer = CaptionImageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            output = replicate.run(
-                "rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
-                input={"image": open(
-                    f'{os.getcwd()}/Seegnal_backend/{serializer.data["image"][1:]}', 'rb')}
-            )
-            trans_output = translator.translate(
-                output, src='en', dest='ko'
-            ).text
-
-            return Response({'text': trans_output}, status=201)
-        else:
-            return Response(serializer.errors, status=400)
-
-
-class CaptionImageUploadENView(APIView):
-    parser_classes = [MultiPartParser, FormParser]
-
-    def post(self, request):
-        serializer = CaptionImageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            output = replicate.run(
-                "rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
-                input={"image": open(
-                    f'{os.getcwd()}/Seegnal_backend/{serializer.data["image"][1:]}', 'rb')}
-            )
-
             return Response({'text': output}, status=201)
         else:
             return Response(serializer.errors, status=400)
