218100d54f0a33bec8444ed3fbcf60ec2943e242
oshmos <ojames9707@gmail.com>
2023-05-17 10:01:52 +0900

    checkout v1 : save file

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,4 @@ from django.urls import path
 urlpatterns = [
     path('v1/caption', CaptionImageUploadView.as_view(), name='caption'),
     path('v1/caption_en', CaptionImageUploadENView.as_view(), name='caption_en'),
-    path('v2/caption_kr', ImageCaptioningKRView.as_view()),
-    path('v2/caption_en', ImageCaptioningENView.as_view()),
 ]
